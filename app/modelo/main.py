f4efe25d5871d0a8ac9e244bee70dc77bb99eea4
iBrokeTheCode <alexrtc.dev@gmail.com>
2025-10-03 09:37:13 -0500

    refactor: Add exception handling to avoid interrupting model execution

--- app/modelo/main.py
+++ app/modelo/main.py
@@ -1,15 +1,19 @@
-from yolo import predict_gap, predict_product
-import redis
 import json
+
+import redis
+from yolo import predict_gap, predict_product
 
 r = redis.Redis(host="localhost", port=6379, db=0)
 
 while True:
-    task = r.brpop("task_queue")  # espera hasta que haya tarea
-    _, value = task
-    data = json.loads(value.decode())  # convierte de JSON a dict
-    prediction_product = predict_product(data['path'])
-    prediction_gap = predict_gap(data['path'])
-    result = {"output_product": prediction_product , "output_gap": prediction_gap}
-    # Guardás el resultado en Redis
-    r.set(data['id'], json.dumps(result))
+    try:
+        task = r.brpop("task_queue")  # espera hasta que haya tarea
+        _, value = task
+        data = json.loads(value.decode())  # convierte de JSON a dict
+        prediction_product = predict_product(data["path"])
+        prediction_gap = predict_gap(data["path"])
+        result = {"output_product": prediction_product, "output_gap": prediction_gap}
+        # Guardás el resultado en Redis
+        r.set(data["id"], json.dumps(result))
+    except Exception as e:
+        print(e)
