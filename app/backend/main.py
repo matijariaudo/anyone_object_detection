1ee9e5e13a1c50fb43fd8d61e03ac99143fbd0bb
iBrokeTheCode <alexrtc.dev@gmail.com>
2025-10-03 20:21:28 -0500

    chore: Change localhost to redis container and fix error in enqueue_image function

--- app/backend/main.py
+++ app/backend/main.py
@@ -1,12 +1,27 @@
-from utils.delete_temp_file import cleanup_old_files
-from utils.duplicates_control import image_differs
+import json
+import os
+import shutil
+import tempfile
+import time
+
+import redis
+import uvicorn
+from database.functions import (
+    check_password,
+    delete_detection,
+    get_cameras_by_user,
+    get_detections,
+    insert_camera,
+    insert_detection,
+    insert_user,
+    update_camera_status,
+)
 from database.install import create_database
-from database.functions import insert_user, check_password, insert_camera, get_cameras_by_user, update_camera_status, get_detections , insert_detection,delete_detection
-from fastapi import FastAPI, Request, UploadFile, File , Form
+from fastapi import FastAPI, File, Form, Request, UploadFile
 from fastapi.responses import HTMLResponse, JSONResponse
 from fastapi.staticfiles import StaticFiles
-import redis, json, tempfile, shutil, os, uvicorn , uuid , time
-
+from utils.delete_temp_file import cleanup_old_files
+from utils.duplicates_control import image_differs
 
 create_database()
 app = FastAPI()
@@ -15,11 +30,16 @@ app = FastAPI()
 frontend_path = os.path.join(os.path.dirname(__file__), "..", "frontend")
 app.mount("/static", StaticFiles(directory=frontend_path), name="static")
 
+r = redis.Redis(host="redis", port=6379, db=0)
+r.delete("task_queue")
+
+
 @app.get("/", response_class=HTMLResponse)
 async def serve_index():
     index_file = os.path.join(frontend_path, "index.html")
     with open(index_file, "r", encoding="utf-8") as f:
         return f.read()
+
 
 @app.post("/test")
 async def predecir(request: Request):
@@ -27,45 +47,107 @@ async def predecir(request: Request):
     # Acá podrías usar el body si necesitas
     return JSONResponse(content={"status": 200, "prediction": True})
 
-r = redis.Redis(host="localhost", port=6379, db=0)
-r.delete("task_queue")
+
+# @app.post("/predict")
+# async def enqueue_image(file: UploadFile = File(...), camera_id: int = Form(...)):
+#     # Crear archivo temporal
+#     # base_tmp = tempfile.gettempdir()
+#     # tmp_dir = os.path.join(base_tmp, "images")
+
+#     SHARED_DIR = "/shared/images"
+#     os.makedirs(SHARED_DIR, exist_ok=True)
+
+#     file_name = f"{unique_id}.{ext}"
+#     tmp_path = os.path.join(SHARED_DIR, file_name)
+
+#     os.makedirs(tmp_dir, exist_ok=True)
+#     print("Existe?", os.path.exists(tmp_dir))  # True si está creada
+#     print("Ruta:", tmp_dir)
+#     ext = file.filename.split(".")[-1]  # extensión (jpg, png, etc.)
+#     unique_id = f"{camera_id}_{int(time.time())}"
+#     file_name = f"{unique_id}.{ext}"
+#     tmp_path = os.path.join(tmp_dir, file_name)
+#     print(unique_id, "--", tmp_path)
+#     with open(tmp_path, "wb") as buffer:
+#         shutil.copyfileobj(file.file, buffer)
+
+#     # 👇 chequeo antes de poner en cola si ha cambiado
+#     if not image_differs(tmp_path, camera_id, threshold=5.0):
+#         cleanup_old_files(camera_id)
+#         return {"status": "no changes"}
+
+#     # Preparo cola pre - servicio
+#     task = {"id": file_name, "path": tmp_path}
+#     if not file_name:
+#         return {"status": "error", "msg": "Missing 'id' in request"}
+#     r.lpush("task_queue", json.dumps(task))  # Pongo en redis
+
+#     # Espero en cola post - servicio
+#     start = time.time()
+#     while time.time() - start < 3:
+#         value = r.getdel(file_name)
+#         if value:
+#             parsed = json.loads(value.decode())  # <- parsea string a dict
+#             cleanup_old_files(camera_id)  # <- Delete temp path
+#             return JSONResponse(
+#                 content={
+#                     "status": "success",
+#                     "result_product": parsed["output_product"],
+#                     "result_gap": parsed["output_gap"],
+#                     "file_name": file_name,
+#                 }
+#             )
+#         time.sleep(0.1)
+#     return {"status": "No results"}
+
+
 @app.post("/predict")
 async def enqueue_image(file: UploadFile = File(...), camera_id: int = Form(...)):
-    # Crear archivo temporal
-    base_tmp = tempfile.gettempdir()
-    tmp_dir = os.path.join(base_tmp, "images")
-    os.makedirs(tmp_dir, exist_ok=True)
-    print("Existe?", os.path.exists(tmp_dir))   # True si está creada
-    print("Ruta:", tmp_dir)
-    ext = file.filename.split(".")[-1]  # extensión (jpg, png, etc.)
+    # Crear directorio compartido
+    shared_dir = "/shared/images"
+    os.makedirs(shared_dir, exist_ok=True)
+
+    # Asegurar nombre de archivo
+    original_name = file.filename or "upload.jpg"
+    ext = original_name.split(".")[-1] if "." in original_name else "jpg"
+
+    # Generar nombre único
     unique_id = f"{camera_id}_{int(time.time())}"
-    file_name=f"{unique_id}.{ext}"
-    tmp_path = os.path.join(tmp_dir, file_name)
-    print(unique_id,"--",tmp_path)
+    file_name = f"{unique_id}.{ext}"
+    tmp_path = os.path.join(shared_dir, file_name)
+
+    # Guardar archivo subido
     with open(tmp_path, "wb") as buffer:
         shutil.copyfileobj(file.file, buffer)
 
     # 👇 chequeo antes de poner en cola si ha cambiado
-    if not image_differs(tmp_path,camera_id, threshold=5.0):
+    if not image_differs(tmp_path, camera_id, threshold=5.0):
         cleanup_old_files(camera_id)
         return {"status": "no changes"}
-    
-    #Preparo cola pre - servicio
+
+    # Preparo cola pre - servicio
     task = {"id": file_name, "path": tmp_path}
-    if not file_name:
-        return {"status": "error", "msg": "Missing 'id' in request"}
-    r.lpush("task_queue", json.dumps(task)) #Pongo en redis
-    
-    #Espero en cola post - servicio
+    r.lpush("task_queue", json.dumps(task))  # Pongo en redis
+
+    # Espero en cola post - servicio
     start = time.time()
     while time.time() - start < 3:
         value = r.getdel(file_name)
         if value:
-            parsed = json.loads(value.decode()) # <- parsea string a dict
-            cleanup_old_files(camera_id)        # <- Delete temp path
-            return JSONResponse(content={"status": "success", "result_product": parsed["output_product"], "result_gap": parsed["output_gap"],"file_name":file_name})
+            parsed = json.loads(value.decode())
+            cleanup_old_files(camera_id)
+            return JSONResponse(
+                content={
+                    "status": "success",
+                    "result_product": parsed["output_product"],
+                    "result_gap": parsed["output_gap"],
+                    "file_name": file_name,
+                }
+            )
         time.sleep(0.1)
+
     return {"status": "No results"}
+
 
 @app.post("/create_account")
 async def create_account(request: Request):
@@ -114,6 +196,7 @@ async def delete_camera(request: Request):
     except Exception as e:
         return {"status": "error", "msg": str(e)}
 
+
 @app.post("/insert_detection")
 async def insert_detection_endpoint(request: Request):
     data = await request.json()
@@ -121,13 +204,15 @@ async def insert_detection_endpoint(request: Request):
         camera_id = data["camera_id"]
         file_name = data["file_name"]
         objects = json.dumps(data.get("objects", []))  # guardamos como texto
-        gaps = json.dumps(data.get("gaps", []))          # idem
+        gaps = json.dumps(data.get("gaps", []))  # idem
 
         # copiar archivo a carpeta fija
         base_tmp = tempfile.gettempdir()
         tmp_path = os.path.join(base_tmp, "images", file_name)
 
-        images_dir = os.path.join(os.path.dirname(__file__), "..", "frontend","imagenes")
+        images_dir = os.path.join(
+            os.path.dirname(__file__), "..", "frontend", "imagenes"
+        )
         os.makedirs(images_dir, exist_ok=True)
         final_path = os.path.join(images_dir, file_name)
 
@@ -140,7 +225,8 @@ async def insert_detection_endpoint(request: Request):
         return {"status": 200, "msg": "Detection saved successfully", "file": file_name}
     except Exception as e:
         return {"status": "error", "msg": str(e)}
-    
+
+
 @app.post("/get_detections")
 async def get_detections_endpoint(request: Request):
     data = await request.json()
@@ -150,7 +236,8 @@ async def get_detections_endpoint(request: Request):
         return {"status": 200, "detections": detections}
     except Exception as e:
         return {"status": "error", "msg": str(e)}
-    
+
+
 @app.post("/delete_detection")
 async def delete_detection_endpoint(request: Request):
     data = await request.json()
@@ -160,6 +247,6 @@ async def delete_detection_endpoint(request: Request):
     except Exception as e:
         return {"status": "error", "msg": str(e)}
 
-if __name__ == "__main__":
-    uvicorn.run("main:app", host="127.0.0.1", port=9000, reload=True)
 
+if __name__ == "__main__":
+    uvicorn.run("main:app", host="0.0.0.0", port=9000, reload=True)
