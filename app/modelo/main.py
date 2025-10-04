3048ff56f054a089686fb0e7e4b9171d94ca4802
iBrokeTheCode <alexrtc.dev@gmail.com>
2025-10-03 20:19:58 -0500

    chore: Change localhost to redis container server

--- app/modelo/main.py
+++ app/modelo/main.py
@@ -1,13 +1,15 @@
-from yolo import predict
-import redis, json
+import json
 
-r = redis.Redis(host="localhost", port=6379, db=0)
+import redis
+from yolo import predict
+
+r = redis.Redis(host="redis", port=6379, db=0)
 
 while True:
     _, value = r.brpop("task_queue")
     data = json.loads(value.decode())
 
-    product_boxes, gap_boxes = predict(data['path'])
+    product_boxes, gap_boxes = predict(data["path"])
     result = {"output_product": product_boxes, "output_gap": gap_boxes}
 
-    r.set(data['id'], json.dumps(result))
+    r.set(data["id"], json.dumps(result))
